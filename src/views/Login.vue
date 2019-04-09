2afdfab0335d6e5090b1bb14c9e9656c98596b32
Kelani Tolulope <kelanik8@gmail.com>
2019-04-09 18:05:56 +0100

    login and registration page built

---
+++ src/views/Login.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="home">
+        <Navbar>
+        </Navbar>
+        <h1>Login</h1>
+    </div>
+</template>
+
+<script>
+    import Navbar from '@/components/Navbar.vue';
+    import { mapState, mapActions, mapGetters } from 'vuex';
+
+    export default {
+        name: 'Login',
+        components: {
+            Navbar
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Registration.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="home">
+        <Navbar>
+        </Navbar>
+        <h1>Registration</h1>
+    </div>
+</template>
+
+<script>
+    import Navbar from '@/components/Navbar.vue';
+    import { mapState, mapActions, mapGetters } from 'vuex';
+
+    export default {
+        name: 'Registration',
+        components: {
+            Navbar
+        }
+    }
+</script>
+
+<style>
+
+</style>
