8f7a39a033c099558151ceceb5912a5fd71280e0
Kelani Tolulope <kelanik8@gmail.com>
2019-04-10 15:47:50 +0100

    getting ready for deployment

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+// const cors = require('cors');
+const path = require('path');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(cors());
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(__dirname + '/public/'));
+    app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html') ));
+  }
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => (console.log(`:>Server started on Port ${PORT}`)));
