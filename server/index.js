792a6506ab448798aaea4bf417ce56a9be98ff09
Elusoji Sodeeq <omoelu1@gmail.com>
2019-05-10 22:33:19 +0100

    modify server/index.js to adjust where app is served from

--- server/index.js
+++ server/index.js
@@ -7,8 +7,10 @@ app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser());
 // app.use(cors());
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(__dirname + '/public/'));
-    app.get('/*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html') ));
+    // app.use(express.static(__dirname + '/public/'));
+    // app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html') ));
+    app.use(express.static(__dirname + '/dist/'));
+    app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html') ));
   }
 const PORT = process.env.PORT || 5000;
 
