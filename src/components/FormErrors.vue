29cac3ed6ae7a658854e35bc37368d5e06b3c3ee
Elusoji Sodeeq <omoelu1@gmail.com>
2019-05-10 17:34:31 +0100

    add formerrors compoenent to display validaition errors

---
+++ src/components/FormErrors.vue
@@ -0,0 +1,26 @@
+<template>
+    <div v-if="validationErrors">
+        <ul class="alert alert-danger">
+            <li v-for="(value, key, index) in validationErrors" :key="index">{{ value }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "FormErrors",
+    data(){
+        return {
+            
+        }
+    },
+    props: ['errors'],
+    computed: {
+        validationErrors(){
+            let errors = Object.values(this.errors);
+            errors = errors.flat();
+            return errors;
+        }
+    }
+}
+</script>
