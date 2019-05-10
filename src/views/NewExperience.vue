f03810f39d8487b2ccddc2bb7f68463a33e8efe9
Kelani Tolulope <kelanik8@gmail.com>
2019-05-10 18:58:45 +0100

    mapstate

--- src/views/NewExperience.vue
+++ src/views/NewExperience.vue
@@ -129,7 +129,7 @@
 <script>
 	import Navbar from '@/components/Navbar.vue';
 	import Footer from '@/components/Footer.vue';
-	import { mapActions } from 'vuex';
+	import { mapActions, mapState } from 'vuex';
 
 	export default {
 		name: 'NewExperience',
