14fe3cad97ba7af4a1e33b1eac18dc7baf9b4f5e
Elusoji Sodeeq <omoelu1@gmail.com>
2019-05-10 21:51:25 +0100

    modify get path in server/index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: false }));
 // app.use(cors());
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(__dirname + '/public/'));
-    app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html') ));
+    app.get('/*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html') ));
   }
 const PORT = process.env.PORT || 5000;
 
